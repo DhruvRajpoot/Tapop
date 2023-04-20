86248e56ca8ba3ea88b8366609934a01626cf6e2
DhruvRajpoot <dhruv.kutarya@gmail.com>
2023-04-20 21:31:43 +0530

    fix: ref error using forward ref

--- src/View/Hero/Hero.jsx
+++ src/View/Hero/Hero.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import ImageView from '../ImageView/ImageView';
 import VideoView from '../VideoView/VideoView';
 import { HeroContainer } from './HeroStyle';
 
 const Hero = () => {
+  const eleRef=useRef(null);
+  const heroRef=useRef(null);
+
+  
+  const transformImage = (e) => {
+    const contraintX = 20;
+    const contraintY = 30;
+    const { clientX, clientY } = e;
+    const { width, height } = window.screen;
+    const xCenter = clientX - width / 2;
+    const yCenter = clientY - height / 2;
+    const xDeg = (yCenter / height) * contraintX;
+    const yDeg = (xCenter / width) * contraintY;
+    eleRef.current.style.transform = `perspective(500px) rotateX(${xDeg}deg) rotateY(${yDeg}deg)`;
+  };
+
+  useEffect(() => {
+    heroRef.current.addEventListener("mousemove", transformImage);
+    heroRef.current.addEventListener("touchmove", transformImage);
+    //eslint-disable-next-line
+  }, []);
+
+  
   return (
-    <HeroContainer>
+    <HeroContainer ref={heroRef}>
       <VideoView/>
-      <ImageView/>
+      <ImageView ref={eleRef}/>
     </HeroContainer>
   )
 }
--- src/View/ImageView/ImageView.jsx
+++ src/View/ImageView/ImageView.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { forwardRef, useRef, useState } from "react";
 import {
   Image,
   ImageViewContainer,
@@ -8,29 +8,10 @@ import {
 import image1 from "../../assets/images/image1.png";
 import image2 from "../../assets/images/image2.png";
 
-const ImageView = () => {
-  const imgContainer = useRef(null);
-  const img = useRef(null);
+const ImageView = forwardRef((props,ref) => {
   const frontImg = useRef(null);
   const backImg = useRef(null);
   const [angle, setAngle] = useState(180);
-
-  const transformImage = (e) => {
-    const contraintX = 20;
-    const contraintY = 30;
-    const { clientX, clientY } = e;
-    const { width, height } = window.screen;
-    const xCenter = clientX - width / 2;
-    const yCenter = clientY - height / 2;
-    const xDeg = (yCenter / height) * contraintX;
-    const yDeg = (xCenter / width) * contraintY;
-    img.current.style.transform = `perspective(500px) rotateX(${xDeg}deg) rotateY(${yDeg}deg)`;
-  };
-
-  useEffect(() => {
-    document.body.addEventListener("mousemove", transformImage);
-    document.body.addEventListener("touchmove", transformImage);
-  }, []);
 
   const rotate = () => {
     frontImg.current.style.transform = `rotateY(${angle}deg)`;
@@ -39,13 +20,13 @@ const ImageView = () => {
   };
 
   return (
-    <ImageViewContainer ref={imgContainer}>
-      <ImageWrapper ref={img} onClick={rotate}>
+    <ImageViewContainer>
+      <ImageWrapper ref={ref} onClick={rotate}>
         <Image src={image1} alt="image" ref={frontImg} />
         <BackImage src={image2} alt="image" ref={backImg} />
       </ImageWrapper>
     </ImageViewContainer>
   );
-};
+});
 
 export default ImageView;
