a230bfec1005f9b71e610c557759a35339bd27ed
DhruvRajpoot <dhruv.kutarya@gmail.com>
2023-04-20 22:01:03 +0530

    fix: flip rotation

--- src/View/ImageView/ImageViewStyle.jsx
+++ src/View/ImageView/ImageViewStyle.jsx
@@ -11,25 +11,20 @@ export const ImageViewContainer = styled.div`
 export const ImageWrapper = styled.button`
   outline: none;
   border: none;
-  width: calc(15rem + 5vw);
+  width: calc(15rem + 8vw);
   height: 80%;
   transform: scale(1);
   padding: 0 1rem;
   position: relative;
-  border-radius: 10px;
-  background: rgb(224, 161, 255);
-  background: linear-gradient(
-    144deg,
-    rgba(224, 161, 255, 0.5540266106442577) 53%,
-    rgba(161, 118, 255, 0.7044467787114846) 100%
-  );
-  box-shadow: 0 0 10px rgb(24, 5, 47);
+  background:transparent;
+  
 `;
 
 export const Image = styled.img`
   padding: 1rem;
   backface-visibility: hidden;
   width: 100%;
+  height: 100%;
   position: absolute;
   top: 0;
   bottom: 0;
@@ -38,6 +33,14 @@ export const Image = styled.img`
   margin: auto;
   transition: transform 0.5s;
   filter: drop-shadow(0 0 8px rgb(24, 5, 47));
+  border-radius: 10px;
+  background: rgb(224, 161, 255);
+  background: linear-gradient(
+    144deg,
+    rgba(224, 161, 255, 0.1540266106442577) 53%,
+    rgba(161, 118, 255, 0.6044467787114846) 100%
+  );
+  box-shadow: 0 0 10px rgb(24, 5, 47);
 `;
 
 export const BackImage = styled(Image)`
